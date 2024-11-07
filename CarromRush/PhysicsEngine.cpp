1aa3ffdf7ecf344103defbaa81d9a7a76020630a
Phạm Triều Dương <phamduong1132005@gmail.com>
2024-11-07 16:53:29 +0700

    feat: add border, collision check for border and apply mass for object.

--- CarromRush/PhysicsEngine.cpp
+++ CarromRush/PhysicsEngine.cpp
@@ -1,16 +1,16 @@
-#include "PhysicsEngine.h"
+﻿#include "PhysicsEngine.h"
 #include "Circle.h"
 #include <iostream>
 namespace BanZ {
 
-	bool PhysicsEngine::CheckCollisionCircleVsCircle(const BanZ::VECTOR2& posCircle1, const BanZ::VECTOR2& posCircle2, const BanZ::VECTOR2& velocityCircle1, const BanZ::VECTOR2& veleocityCircle2, const float& radiusCircle1, const float& radiusCircle2)
+	bool PhysicsEngine::CheckCollisionCircleVsCircle(const BanZ::VECTOR2& posCircle1, const BanZ::VECTOR2& posCircle2, const float& radiusCircle1, const float& radiusCircle2)
 	{
 		auto banGame = BanGame::Get();
 		float distance = banGame->Length(posCircle1 - posCircle2);
 		return distance < radiusCircle1 + radiusCircle2;
 	}
 
-	void PhysicsEngine::HandleCollisionCircleVsCircle(BanZ::VECTOR2& posCircle1, BanZ::VECTOR2& posCircle2, BanZ::VECTOR2& velocityCircle1, BanZ::VECTOR2& velocityCircle2, const float& radiusCircle1, const float& radiusCircle2)
+	void PhysicsEngine::HandleCollisionCircleVsCircle(BanZ::VECTOR2& posCircle1, BanZ::VECTOR2& posCircle2, BanZ::VECTOR2& velocityCircle1, BanZ::VECTOR2& velocityCircle2, const float& radiusCircle1, const float& radiusCircle2, const float& mass1, const float& mass2)
 	{
 		auto banGame = BanGame::Get();
 		VECTOR2 collisionDir = banGame->Normalize(posCircle1 - posCircle2);
@@ -23,25 +23,106 @@ namespace BanZ {
 		float v1 = banGame->Dot(velocityCircle1, collisionDir);
 		float v2 = banGame->Dot(velocityCircle2, collisionDir);
 
-		float newV1 = v2;	// (velocityCircle1 * (m1 - m2) + 2 * m2 * velocityCircle2) / (m1 + m2);   m1=m2
-		float newV2 = v1;	//(velocityCircle2 * (m2 - m1) + 2 * m1 * velocityCircle1) / (m1 + m2);
+		float newV1 = (v1 * (mass1 - mass2) + 2 * mass2 * v2) / (mass1 + mass2);
+		float newV2 = (v2 * (mass2 - mass1) + 2 * mass1 * v1) / (mass1 + mass2);
 
 		velocityCircle1 += collisionDir * (newV1 - v1);
 		velocityCircle2 += collisionDir * (newV2 - v2);
+
+	}
+	VECTOR2 FindNearestPointOnRectToCircle(const VECTOR2& circlePos, const VECTOR2& rectPos, const VECTOR2& rectEdge)
+	{
+		float halfWidth = rectEdge.x / 2.0f;
+		float halfHeight = rectEdge.y / 2.0f;
+
+		float nearestX;
+		if (circlePos.x < rectPos.x - halfWidth) {
+			nearestX = rectPos.x - halfWidth;
+		}
+		else if (circlePos.x > rectPos.x + halfWidth) {
+			nearestX = rectPos.x + halfWidth;
+		}
+		else {
+			nearestX = circlePos.x;
+		}
+
+		float nearestY;
+		if (circlePos.y < rectPos.y - halfHeight) {
+			nearestY = rectPos.y - halfHeight;
+		}
+		else if (circlePos.y > rectPos.y + halfHeight) {
+			nearestY = rectPos.y + halfHeight;
+		}
+		else {
+			nearestY = circlePos.y;
+		}
+
+		return VECTOR2{ nearestX, nearestY };
+	}
+	bool PhysicsEngine::CheckCollisionCircleVsBorder(const BanZ::VECTOR2& posCircle, const float& radiusCircle, const BanZ::VECTOR2& posBorder, const BanZ::VECTOR2& borderEdge,VECTOR2& collisionNormal)
+	{
+		VECTOR2 nearestPoint = FindNearestPointOnRectToCircle(posCircle, posBorder, borderEdge);
+		float distance = BanGame::Get()->Length(posCircle - nearestPoint);
+		if (distance <= radiusCircle)
+		{
+			collisionNormal = BanGame::Get()->Normalize(posCircle - nearestPoint);
+			return true;
+		}
+
+		return false;
+	}
+	//Unity vector2 reflect
+	void PhysicsEngine::HandleCollisionCircleVsBorder(BanZ::VECTOR2& velocityCircle, const VECTOR2 collisionNormal)
+	{
+		float dotProduct = BanGame::Get()->Dot(velocityCircle, collisionNormal);
+		velocityCircle = velocityCircle- collisionNormal * (2 * dotProduct);
 	}
 	void PhysicsEngine::ProcessCollision()
 	{
 		auto ActorManager = ActorManager::getInstance();
 		auto actors = ActorManager->GetActors();
-		for (int i = 0; i < actors.size(); i++)
+		std::vector<Circle*> circles;
+		std::vector<Border*> borders;
+
+		for (auto actor : actors)
 		{
-			for (int j = i + 1; j < actors.size(); j++)
+			if (auto circle = dynamic_cast<Circle*>(actor))
+				circles.push_back(circle);
+			else if (auto border = dynamic_cast<Border*>(actor))
+				borders.push_back(border);
+		}
+		CheckCircleVsCircleCollisions(circles);
+	
+		CheckCircleVsBorderCollisions(circles, borders);
+	}
+	void PhysicsEngine::CheckCircleVsCircleCollisions(const std::vector<Circle*>& circles)
+	{
+		for (int i = 0; i < circles.size(); i++)
+		{
+			for (int j = i + 1; j < circles.size(); j++)
 			{
-				auto circle1 = dynamic_cast<Circle*>(actors[i]);
-				auto circle2 = dynamic_cast<Circle*>(actors[j]);
-				if (CheckCollisionCircleVsCircle(circle1->GetPosition(), circle2->GetPosition(), circle1->GetVelocity(), circle2->GetVelocity(), circle1->GetRadius(), circle2->GetRadius()))
+				auto circle1 = circles[i];
+				auto circle2 = circles[j];
+				if (circle1 != nullptr && circle2 != nullptr)
 				{
-					HandleCollisionCircleVsCircle(circle1->GetPosition(), circle2->GetPosition(), circle1->GetVelocity(), circle2->GetVelocity(), circle1->GetRadius(), circle2->GetRadius());
+					if (CheckCollisionCircleVsCircle(circle1->GetPosition(), circle2->GetPosition(), circle1->GetRadius(), circle2->GetRadius()))
+					{
+						HandleCollisionCircleVsCircle(circle1->GetPosition(), circle2->GetPosition(), circle1->GetVelocity(), circle2->GetVelocity(), circle1->GetRadius(), circle2->GetRadius(), circle1->GetMass(), circle2->GetMass());
+					}
+				}
+			}
+		}
+	}
+	void PhysicsEngine::CheckCircleVsBorderCollisions(const std::vector<Circle*>& circles, const std::vector<Border*>& borders)
+	{
+		for (auto circle : circles)
+		{
+			for (auto border : borders)
+			{
+				BanZ::VECTOR2 collisionNormal;
+				if (CheckCollisionCircleVsBorder(circle->GetPosition(), circle->GetRadius(), border->GetPosition(), border->GetBorderEdge(), collisionNormal))
+				{
+					HandleCollisionCircleVsBorder(circle->GetVelocity(), collisionNormal);
 				}
 			}
 		}
